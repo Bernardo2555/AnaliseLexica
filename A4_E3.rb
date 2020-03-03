9b1246d563ed80d9be155ac04a84fac7f481b2c9
Bernardo2555 <bvpereira6@gmail.com>
2020-03-03 12:57:49 +0000

    1parte

---
+++ A4_E3.rb
@@ -0,0 +1,13 @@
+# números reais
+# números inteiros
+# operadores de soma
+# operadores de multiplicação
+# abre parênteses
+# fecha parênteses
+
+ estrutura = '12.1+++81***123.2+1)('
+roda = true
+
+while roda
+    
+end
