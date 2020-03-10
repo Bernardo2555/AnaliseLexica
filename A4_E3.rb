0f34094df16ac4e2a93bdce94189bdceaaae00ee
Bernardo2555 <bvpereira6@gmail.com>
2020-03-10 13:04:04 +0000

    falta separar numeros

--- A4_E3.rb
+++ A4_E3.rb
@@ -3,11 +3,26 @@
 # operadores de soma
 # operadores de multiplicação
 # abre parênteses
-# fecha parênteses
+# fecha parênteses 
 
- estrutura = '12.1+++81***123.2+1)('
-roda = true
-
-while roda
-    
+estrutura = gets.chomp.to_s
+i = 0
+caso = 'A'
+while i < estrutura.bytesize do
+    if estrutura[i] =~ /\A[+-]?\d+(\.\d+)?\z/ #arrumar, ele identifica um numero 
+        puts 'Tipo do simbolo: numero, simbolo: ' + estrutura[i]
+    else
+        if estrutura[i] =~ /[+]/
+            puts 'Tipo do simbolo: +'
+        else
+            if estrutura[i] =~ /[*]/
+                puts 'Tipo do simbolo: *'
+            else
+                if estrutura[i] =~ /[(]/ || estrutura[i] =~ /[)]/
+                    puts 'Tipo do simbolo: (), simbolo: ' + estrutura[i]
+                end
+            end
+        end
+    end
+        i += 1
 end
